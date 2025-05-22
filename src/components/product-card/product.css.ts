3c106b7b5dcc175b4b96e52e0b8151e74998c83e
TS22082 <ts22082@gmail.com>
2025-05-21 21:44:02 -0700

    fix media break points

--- src/components/product-card/product.css.ts
+++ src/components/product-card/product.css.ts
@@ -12,18 +12,17 @@ export const imageContainer = style({
 
 export const cardContainer = style({
   width: "25%",
+  minWidth: "300px",
   boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
   cursor: "pointer",
-  padding: "0px",
-  margin: "0px",
   border: "none",
   backgroundColor: "transparent",
   "@media": {
-    "screen and (max-width: 768px)": {
+    "screen and (max-width: 1000px)": {
       width: "40%",
     },
     "screen and (max-width: 480px)": {
-      width: "300px",
+      width: "80%",
     },
   },
 });
